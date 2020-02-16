5ec1fbf870083ba18ae68499dd8a6a4b8d8666af
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2020-02-16 20:52:06 +0000

    Script to add static data to database (already executed), and accompanying static csv file

---
+++ DB_setup/api2db_static.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import json, urllib.request
+from sqlalchemy import create_engine
+import sys
+
+
+host = "rljdb.ctx6gghqwipv.us-east-1.rds.amazonaws.com"
+#port=3306
+dbname = "DublinBikes"
+user="admin"
+password="chuckberry69"
+
+db_connection = "mysql+pymysql://{0}:{1}@{2}/{3}".format(user, password, host, dbname)
+engine = create_engine(db_connection)
+
+df = pd.read_csv('dbikes_static.csv')
+
+df.to_sql('bikes_static', con=engine, if_exists='replace', index=False)
