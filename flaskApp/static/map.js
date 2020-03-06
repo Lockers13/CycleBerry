973422b624c0f9a4554d008273457a302634f5f4
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2020-03-06 11:39:45 +0000

    Added markers for each bike stand

--- flaskApp/static/map.js
+++ flaskApp/static/map.js
@@ -1,22 +1,57 @@
-		
+
 // Initialize and add the map
-function initMap() {
-	var dublin = {lat: 53.3498, lng: -6.2603};
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 12, center: dublin});
-  var marker = new google.maps.Marker({position: dublin, map: map});
-  }
+var url = 'http://localhost:5000/api/coordinates';
 
-  $(document).ready(function() {
-      console.log("Hello");
-      fetch('http://localhost:5000/api/coordinates').then(function(response) {
-        // The response is a Response instance.
-        // You parse the data into a useable format using `.json()`
-        return response.json();
-      }).then(function(data) {
-        // `data` is the parsed version of the JSON returned from the above endpoint.
-        console.log(data);  // { "userId": 1, "id": 1, "title": "...", "body": "..." }
-      });
-  })
+function initMap(data) {
+  return fetch(url)
+    .then(response => response.json())
+    .then(function (data) {
+      var dublin = { lat: 53.3498, lng: -6.2603 };
+      var map = new google.maps.Map(
+        document.getElementById('map'), {
+          zoom: 12,
+          center: dublin,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        var infowindow = new google.maps.InfoWindow();
+        var marker, i;
+        var coord_len = data.coordinates.length;
+        for (i = 0; i < coord_len; i++) {
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(data.coordinates[i].lat, data.coordinates[i].lng),
+            map: map
+          });
+          google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+              infowindow.setContent(data.coordinates[i].name);
+              infowindow.open(map, marker);
+            }
+          })(marker, i));
+        }
+    });
+}
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
+/*$(document).ready(function () {
+  getData(url);// { "userId": 1, "id": 1, "title": "...", "body": "..." }
+});*/
+
+
+
+/*function getData(url) {
+  console.log("hello");
+}*/
+
+
+
