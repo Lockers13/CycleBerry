d3ebee9c1801355d424a94a1aea8b0b0c30a9eac
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2020-03-06 11:58:17 +0000

    Added markers for each bike stand

--- flaskApp/static/map.js
+++ flaskApp/static/map.js
@@ -9,21 +9,37 @@ function initMap(data) {
       var dublin = { lat: 53.3498, lng: -6.2603 };
       var map = new google.maps.Map(
         document.getElementById('map'), {
-          zoom: 12,
+          zoom: 14,
           center: dublin,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         });
         var infowindow = new google.maps.InfoWindow();
         var marker, i;
+        var shape = {
+          coords: [1, 1, 1, 20, 18, 20, 18, 1],
+          type: 'poly'
+        };
+        var image = {
+          url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+          // This marker is 20 pixels wide by 32 pixels high.
+          size: new google.maps.Size(20, 32),
+          // The origin for this image is (0, 0).
+          origin: new google.maps.Point(0, 0),
+          // The anchor for this image is the base of the flagpole at (0, 32).
+          anchor: new google.maps.Point(0, 32)
+        };
         var coord_len = data.coordinates.length;
         for (i = 0; i < coord_len; i++) {
           marker = new google.maps.Marker({
             position: new google.maps.LatLng(data.coordinates[i].lat, data.coordinates[i].lng),
-            map: map
+            map: map,
+            icon: image,
+            shape: shape,
+            title: "CycleBerry"
           });
           google.maps.event.addListener(marker, 'click', (function (marker, i) {
             return function () {
-              infowindow.setContent(data.coordinates[i].name);
+              infowindow.setContent(data.coordinates[i].num + ": " + data.coordinates[i].name);
               infowindow.open(map, marker);
             }
           })(marker, i));
@@ -52,6 +68,10 @@ function initMap(data) {
 /*function getData(url) {
   console.log("hello");
 }*/
+
+
+
+
 
 
 
