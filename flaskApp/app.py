487d7e2896dad5b0c3d7bb98bd89f2b0102f0c59
Ronan Byrne <ronan.byrne1@ucdconnect.ie>
2020-03-09 17:10:34 +0000

    Added weather data and dynamic bike data to flask app

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -5,18 +5,43 @@ import sys
 from mysql.connector import Error
 from flask import jsonify
 from flask import render_template, flash, redirect, url_for
-
-
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+# Config MySql
+app.config['MYSQL_HOST'] = 'rljdb.cgvwbmfcg1yd.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'chuckberry69'
+app.config['MYSQL_DB'] = 'DublinBikes'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#Initialise SQL
+mysqlinit = MySQL(app)
+
 #Allows for changes without restarting the server#
-#app.debug = True
+app.debug = True
+
+#Handy tool to see how templates are being registered
+#May need to use pip install flask-debugtoolbar
+app.config['SECRET_KEY'] = "123"
+toolbar = DebugToolbarExtension(app)
 
 
 @app.route('/')
 def index():
-	return render_template('maps.html')
+	mycursor = mysqlinit.connection.cursor()
+
+	mycursor.execute("SELECT * FROM testWeather ORDER BY dt DESC LIMIT 1")
+
+	myResult = mycursor.fetchall()
+
+	mycursor.execute("SELECT * FROM dynamic where last_update IN (select MAX(last_update)From DublinBikes.dynamic group by number);")
+
+	dynamicBike = mycursor.fetchall()
+
+	return render_template('maps.html', dynamicBike=dynamicBike , myResult=myResult)
 
 
 @app.route('/api/coordinates')
@@ -52,3 +77,4 @@ def coordinates():
 
 if __name__ == '__main__':
 	app.run()
+	app.about()
